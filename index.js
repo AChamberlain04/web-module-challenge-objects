d3ea632cd602c3420b5361abe2f47283d7a37435
Adam Chamberlain <a.chamberlain04@gmail.com>
2022-08-28 05:40:02 -0400

    task 1 done

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ The function should:
 */
 
 
-function createMenuItem(/*Your code here*/){
-  /*Your code here*/
+function createMenuItem(name, price, category){
+  return {name, price, category};
 }
+console.log(createMenuItem('breakfast Burrito', 16, 'Breakfast'));
 
 
 
