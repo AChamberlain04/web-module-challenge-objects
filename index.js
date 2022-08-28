2c2cad3006bbc682771c9beb94516a7929c3f20d
Adam Chamberlain <a.chamberlain04@gmail.com>
2022-08-28 05:55:27 -0400

    task 2

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
-
+console.log(createMenuItem("pizza", 5, "Lunch")); 
+console.log(createMenuItem("BLT", 4, "Lunch 2"));
+console.log(createMenuItem("Steak and Lobster", 29, "Dinner"));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the burger object below that automatically calculates price depending on the string received as a parameter. 
